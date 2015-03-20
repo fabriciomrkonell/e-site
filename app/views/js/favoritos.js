dcbb1bdd7944a011fb5b4c6d4e3975860c94e04c
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-03-20 17:41:50 -0300

    Up

--- app/views/js/favoritos.js
+++ app/views/js/favoritos.js
@@ -8,6 +8,11 @@
 		var cookie = getProdutosCookie(),
         produtos = "?init=true";
 
+    if(cookie.length < 1){
+      document.getElementById('message').style.display = "";
+      document.getElementById('message').innerHTML = "Nenhum produto encontrado!";
+    }
+
     for(var i = 0; i < cookie.length; i++){
       produtos = produtos + "&" + _array[i] + '=' + cookie[i];
     }
@@ -37,15 +42,16 @@
     }
     ajax.open("GET", "/api/favoritos" + url, true );
     ajax.onreadystatechange = function () {
+      document.getElementById("all-products").innerHTML = "";
       if(ajax.readyState == 1){
         document.getElementById('message').innerHTML = "Carregando!";
       }
       if(ajax.readyState == 4){
         if(ajax.status == 200){
           if(JSON.parse(ajax.responseText).length > 0){
-            document.getElementById('message').style.display = "none";
-            document.getElementById("all-products").innerHTML = "";
+            document.getElementById('message').innerHTML = "Nenhum produto encontrado!";
             for(var i = 0; i < JSON.parse(ajax.responseText).length; i++){
+              document.getElementById('message').style.display = "none";
               document.getElementById("all-products").innerHTML = document.getElementById("all-products").innerHTML + "<div class='product-box'><div class='product-box__wrap'><span class='product-box__favorite__star product-box__favorite__star-active' onclick='unstar(" + JSON.parse(ajax.responseText)[i].Produto.id + ")'>&#9733;</span><img src='" + JSON.parse(ajax.responseText)[i].Produto.imagem + "' class='product-box__img'><section class='product-description'><div class='product-name line-clamp'><p class='product-name__text'>" + JSON.parse(ajax.responseText)[i].Produto.descricao + "</p></div><span class='product-price'>" + JSON.parse(ajax.responseText)[i].Produto.valor + "</span></section></div></div>";
             };
           }else{
